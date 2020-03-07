58294985680027063c50fbf3c7c00f1d5bf32f0a
georlav <georlav@gmail.com>
2020-03-07 12:25:37 +0200

    model responses

--- internal/recipe/recipe.go
+++ internal/recipe/recipe.go
@@ -3,14 +3,14 @@ package recipe
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Recipe struct {
-	ID          string             `json:"id" bson:"-"`
-	MID         primitive.ObjectID `json:"-" bson:"_id,omitempty"`
-	Title       string             `json:"title" bson:"title"`
-	URL         string             `json:"url" bson:"url"`
-	Ingredients []string           `json:"ingredients" bson:"ingredients"`
-	Thumbnail   string             `json:"thumbnail" bson:"thumbnail"`
-	UpdatedAt   string             `json:"updatedAt" bson:"-"`
-	CreatedAt   string             `json:"createdAt" bson:"-"`
+	ID          string             `bson:"-"`
+	MID         primitive.ObjectID `bson:"_id,omitempty"`
+	Title       string             `bson:"title"`
+	URL         string             `bson:"url"`
+	Ingredients []string           `bson:"ingredients"`
+	Thumbnail   string             `bson:"thumbnail"`
+	UpdatedAt   string             `bson:"-"`
+	CreatedAt   string             `bson:"-"`
 }
 
 type Recipes []Recipe

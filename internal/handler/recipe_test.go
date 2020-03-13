ff36a469b4c9e7f81c9d0fc903e605f12864e10a
georlav <georlav@gmail.com>
2020-03-13 13:44:26 +0200

    handler tests

--- internal/handler/recipe_test.go
+++ internal/handler/recipe_test.go
@@ -1,14 +1,11 @@
 package handler_test
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
-	"os"
 	"strings"
 	"testing"
 
@@ -19,59 +16,6 @@ import (
 	"github.com/georlav/recipeapi/internal/config"
 	"github.com/georlav/recipeapi/internal/handler"
 )
-
-func TestMain(m *testing.M) {
-	// load config
-	cfg, err := config.Load("testdata/config.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// load data to import
-	jd, err := ioutil.ReadFile("testdata/recipes.json")
-	if err != nil {
-		log.Fatalf("failed to load test data, %s", err)
-	}
-
-	// Create recipes from data
-	var data struct{ Recipes database.Recipes }
-	if err := json.Unmarshal(jd, &data); err != nil {
-		log.Fatalf("failed to marshal testdata, %s", err)
-	}
-
-	// Get a recipe handle
-	db, err := database.New(cfg.MySQL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Import data
-	for i := range data.Recipes {
-		if _, err := db.Recipe.Insert(data.Recipes[i]); err != nil {
-			log.Fatalf("failed to insert test data, %s", err)
-		}
-	}
-
-	code := m.Run()
-
-	if _, err := db.Handle.Exec(`SET FOREIGN_KEY_CHECKS = 0`); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := db.Handle.Exec(`TRUNCATE TABLE recipe`); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := db.Handle.Exec(`TRUNCATE TABLE ingredient`); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := db.Handle.Exec(`SET FOREIGN_KEY_CHECKS = 1`); err != nil {
-		log.Fatal(err)
-	}
-	if err := db.Handle.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	os.Exit(code)
-}
 
 func TestHandler_Recipe(t *testing.T) {
 	testData := []struct {

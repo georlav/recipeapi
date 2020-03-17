71ef201664751fc045b0c9cf2c4b2c0c903c5656
georlav <georlav@gmail.com>
2020-03-17 11:28:59 +0200

    remove walk

--- internal/handler/route.go
+++ internal/handler/route.go
@@ -1,9 +1,7 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -23,38 +21,12 @@ func Routes(h *Handler) *mux.Router {
 	// user routes
 	r.HandleFunc("/api/user/signin", h.SignIn).Methods(http.MethodPost).Name("signin")
 	r.HandleFunc("/api/user/signup", h.SignUp).Methods(http.MethodPost).Name("signup")
-
 	user := r.Path("/api/user").Subrouter()
 	user.Use(h.AuthorizationMiddleware)
 	user.HandleFunc("", h.User).Methods(http.MethodGet).Name("profile")
 
 	// health endpoint
 	r.HandleFunc("/health", nil).Methods("GET").Name("health")
-
-	r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		pathTemplate, err := route.GetPathTemplate()
-		if err == nil {
-			fmt.Println("ROUTE:", pathTemplate)
-		}
-		pathRegexp, err := route.GetPathRegexp()
-		if err == nil {
-			fmt.Println("Path regexp:", pathRegexp)
-		}
-		queriesTemplates, err := route.GetQueriesTemplates()
-		if err == nil {
-			fmt.Println("Queries templates:", strings.Join(queriesTemplates, ","))
-		}
-		queriesRegexps, err := route.GetQueriesRegexp()
-		if err == nil {
-			fmt.Println("Queries regexps:", strings.Join(queriesRegexps, ","))
-		}
-		methods, err := route.GetMethods()
-		if err == nil {
-			fmt.Println("Methods:", strings.Join(methods, ","))
-		}
-		fmt.Println()
-		return nil
-	})
 
 	return r
 }
